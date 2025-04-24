912d1249cfd365e6b74692d4ae880d1b58efb348
ajeena <ajeena.sics@gmail.com>
2025-04-24 15:19:38 +0530

    new

--- server/Investors/investorController.js
+++ server/Investors/investorController.js
@@ -2,7 +2,7 @@ const Investor =require('./investorSchema');
 const multer=require('multer');
 const jwt = require('jsonwebtoken');
 const secret = 'invester';
-
+const bcrypt=require('bcrypt')
 const storage=multer.diskStorage({
     destination:function(req,res,cb){
         cb(null,"./upload");
@@ -20,71 +20,58 @@ const uploadSingle= multer({storage:storage}).single("profile");
 
 // Register Investor
 
-const registerInvestor= async(req,res)=>{
-    console.log("in Api");
-    try{
-        const {
-            name,
-            email,
-            contact,
-            organization,
-            nationality,
-            password,
-            investing_category,
-            occupation,
-            description,
-            address,
-        }=req.body;
+const registerInvestor = async (req, res) => {
+  console.log("in Api");
+  try {
+      const {
+          name,
+          email,
+          contact,
+          organization,
+          nationality,
+          password,
+          investing_category,
+          occupation,
+          description,
+          address,
+      } = req.body;
 
-        const newInvestor=new Investor({
-            name,
-            email,
-            contact,
-            organization,
-            nationality,
-            password,
-            investing_category,
-            occupation,
-            description,
-            address,
-            profile:req.files[0],
-            identification_document:req.files[1],
-        
-        });
-        let existingInvestor_email = await Investor.findOne({ email });
-        if (existingInvestor_email) {
-            return res.status(409).json({
-                msg: "Email Already Registered With Us !!",
-                data: null
-            });
-        }
-        let existingInvestor_contact = await Investor.findOne({ contact });
-        if (existingInvestor_contact){
-            return res.status(409).json({
-                msg:"Contact Already Exists !!",
-                data: null
-            })
-        }
-        await newInvestor.save()
-        
-        .then(data => {
-            res.status(200).json({
-                msg: "Inserted successfully",
-                data: data
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                msg: "Data not Inserted",
-                data: err
-            });
-        });
-    }catch (error) {
-        console.log("err",error);
-        res.status(500).json({ message: error.message });
-    }
-}
+      let existingInvestor_email = await Investor.findOne({ email });
+      if (existingInvestor_email) {
+          return res.status(409).json({ msg: "Email Already Registered With Us !!", data: null });
+      }
+      let existingInvestor_contact = await Investor.findOne({ contact });
+      if (existingInvestor_contact) {
+          return res.status(409).json({ msg: "Contact Already Exists !!", data: null });
+      }
+
+      // 🔐 Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const newInvestor = new Investor({
+          name,
+          email,
+          contact,
+          organization,
+          nationality,
+          password: hashedPassword,
+          investing_category,
+          occupation,
+          description,
+          address,
+          profile: req.files[0],
+          identification_document: req.files[1],
+      });
+
+      await newInvestor.save()
+          .then(data => res.status(200).json({ msg: "Inserted successfully", data }))
+          .catch(err => res.status(500).json({ msg: "Data not Inserted", data: err }));
+  } catch (error) {
+      console.log("err", error);
+      res.status(500).json({ message: error.message });
+  }
+};
+
 
 // View all investors
 const viewInvestors = (req, res) => {
@@ -298,31 +285,33 @@ const investorBanByAdmin = (req, res) => {
 
 // Login Investor 
 
-const loginInvestor=(req,res)=>{
-    const { email , password }=req.body
-    Investor.findOne({email})
-    .exec()
-    .then(user=>{
-        if(!user){
-            return res.json({status:409,msg:'user not found'})
-        }else if(user.password!==password){
-            return res.json({status:409,msg:'Password Missmatch !!'})
-        }
+const loginInvestor = (req, res) => {
+  const { email, password } = req.body;
+  Investor.findOne({ email })
+      .exec()
+      .then(async user => {
+          if (!user) {
+              return res.json({ status: 409, msg: 'User not found' });
+          }
 
-        const token = createToken(user)
+          const isMatch = await bcrypt.compare(password, user.password);
+          if (!isMatch) {
+              return res.json({ status: 409, msg: 'Password Mismatch !!' });
+          }
 
-        res.json({
-            data:user,
-            status:200,
-            token:token
-        });
-    })
-    .catch(err=>{
-        console.log(err);
-        return res.status(500).json({msg:'Something went wrong'})
-    });
-
+          const token = createToken(user);
+          res.json({
+              data: user,
+              status: 200,
+              token: token
+          });
+      })
+      .catch(err => {
+          console.log(err);
+          return res.status(500).json({ msg: 'Something went wrong' });
+      });
 };
+
 
 // Update investor by ID
 const editInvestorById = (req, res) => {
@@ -371,30 +360,32 @@ const editInvestorById = (req, res) => {
 
 
 // Forgot Password for entrepreneur
-const forgotPassword = (req, res) => {
-  Investor.findOneAndUpdate(
-    { email: req.body.email },
-    {
-      password: req.body.password,
+const forgotPassword = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Hash the new password
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const updatedInvestor = await Investor.findOneAndUpdate(
+      { email },
+      { password: hashedPassword },
+      { new: true } // to return the updated document
+    );
+
+    if (updatedInvestor) {
+      res.status(200).json({ msg: "Password updated successfully" });
+    } else {
+      res.status(404).json({ msg: "User not found" });
     }
-  )
-    .exec()
-    .then((data) => {
-      if (data != null)
-        res.status(200).json({
-          msg: "Updated successfully",
-        });
-      else
-        res.status(500).json({
-          msg: "User Not Found",
-        });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        msg: "Data not Updated",
-        Error: err,
-      });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      msg: "Something went wrong during password update",
+      error: err.message,
     });
+  }
 };
 
 module.exports={
